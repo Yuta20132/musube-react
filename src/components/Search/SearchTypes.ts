fe970477cc29b884b672a01dff9ff1eff9fa6562
himegitaichi <himetai0625@icloud.com>
2024-09-05 20:27:47 +0900

    SearchTypes.tsの作成

---
+++ src/components/Search/SearchTypes.ts
@@ -0,0 +1,11 @@
+//ユーザーコンポーネントの定義
+export interface SearchResult {
+    id: number;
+    firstName: string;
+    lastName: string;
+    userName: string;
+    email: string;
+    institution: string;
+    institutionType: string;
+  }
+  
